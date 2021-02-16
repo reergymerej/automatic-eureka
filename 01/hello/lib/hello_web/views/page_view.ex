2dcac95d89eccad8f99e9074de50ce6aacfb0b24
Jeremy Greer <jex.grizzle@gmail.com>
2021-02-15 22:16:27 -0500

    Move data into view
    Use loops to render the data in the template.  The data management can
    all be in the view.

--- 01/hello/lib/hello_web/views/page_view.ex
+++ 01/hello/lib/hello_web/views/page_view.ex
@@ -1,60 +1,80 @@
 defmodule HelloWeb.PageView do
   use HelloWeb, :view
 
-  def problem1() do
-    "Problem: I am just writing HTML, not Elixir."
+  def solved() do
+    [
+      %{
+        :problem => "It is difficult to keep track of functions and their placement in the markup.",
+        :solution => """
+          Find another data structure to store these.
+          It should be ordered, so we can sort.
+          It should have
+          * problem
+          * solution?
+          If there is a solution, show it in the solved section.  If there is not, show it in unsolved.
+
+          What data structure can we use?
+
+          list: https://elixir-lang.org/getting-started/basic-types.html#linked-lists
+
+          KWL are cute, but only meant for simple objects, lists of single key/value pairs.
+
+          We should use a list of maps.
+          https://elixir-lang.org/getting-started/keywords-and-maps.html#maps
+
+
+          > lists in Phoenix.HTML and templates may only contain integers representing bytes, binaries or other lists, got invalid entry
+          If you want to dump a complex structure into a template, use inspect.
+
+          ```
+          data: <%= inspect data() %>
+          ```
+
+          Fun note, even if you change a template's backing view, the page doesn't reload until you change the template.
+
+          I've separated the list management from the template.  Now we can just manage data in the page_view (01/hello/lib/hello_web/views/page_view.ex).
+        """,
+      },
+      %{
+        :problem => "I am just writing HTML, not Elixir.",
+        :solution =>
+          """
+          Solution: Write the problems/solutions in Elixir.
+          How do I do that?  This is a view.  Views will get data somehow.
+          Correction, this is a template.
+          https://hexdocs.pm/phoenix/views.html#content
+          """
+      },
+      %{
+        :problem => "I need a way to separate solved and unsolved problems." ,
+        :solution => "Solution: Separate the lists." ,
+      },
+      %{
+        :problem => "I have many problems and I need a way to sort them." ,
+        :solution => "Solution: Change the order in the markup." ,
+      },
+      %{
+        :problem => "I need a way to show which problem I am working on." ,
+        :solution => "Solution: Whatever.  No you don't.  That's not part of the app.  The app is the result of the solutions." ,
+      },
+      %{
+        :problem => "I want to show a history of problems and the solutions." ,
+      },
+      %{
+        :problem => "I want the evolution to be something that can be followed." ,
+      },
+      %{
+        :problem => "As I create solutions (MVPs), I will change the content.  I want the history/evolution to be something that can be followed." ,
+        :solution => "After each solution, add a commit." ,
+      },
+    ]
   end
 
-  def solution1() do
-    """
-    Solution: Write the problems/solutions in Elixir.
-    How do I do that?  This is a view.  Views will get data somehow.
-    Correction, this is a template.
-    https://hexdocs.pm/phoenix/views.html#content
-    """
-  end
-
-  def pm1() do
-    "Problem: I need a way to separate solved and unsolved problems."
-  end
-
-  def sm1() do
-    "Solution: Separate the lists."
-  end
-
-  def pm2() do
-    "Problem: I have many problems and I need a way to sort them."
-  end
-
-  def sm2() do
-    "Solution: Change the order in the markup."
-  end
-
-  def pm3() do
-    "Problem: I need a way to show which problem I am working on."
-  end
-
-  def sm3() do
-    "Solution: Whatever.  No you don't.  That's not part of the app.  The app is the result of the solutions."
-  end
-
-  def pm4() do
-    "Problem: I want to show a history of problems and the solutions."
-  end
-
-  def pm5() do
-    "Problem: I want the evolution to be something that can be followed."
-  end
-
-  def pm6() do
-    "Problem: As I create solutions (MVPs), I will change the content.  I want the history/evolution to be something that can be followed."
-  end
-
-  def sm6() do
-    "Solution: After each solution, add a commit."
-  end
-
-  def p2() do
-    "Problem: I want a way to merge similar problems."
+  def unsolved() do
+    [
+      %{
+        :problem => "I want a way to merge similar problems.",
+      },
+    ]
   end
 end
